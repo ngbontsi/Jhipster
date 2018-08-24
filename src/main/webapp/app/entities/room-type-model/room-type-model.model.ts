7e966bcb810098d3ddfe24d6d55cb6beb05011c4
ndimphiwe.bontsi <ndimphiwe.bontsi@metacom.co.za>
2018-08-24 16:20:45 +0200

    cleaning application layout

--- src/main/webapp/app/entities/room-type-model/room-type-model.model.ts
+++ src/main/webapp/app/entities/room-type-model/room-type-model.model.ts
@@ -3,9 +3,7 @@ import { BaseEntity } from './../../shared';
 export class RoomTypeModel implements BaseEntity {
     constructor(
         public id?: number,
-        public roomtype?: number,
         public description?: string,
-        public rates?: number,
     ) {
     }
 }
